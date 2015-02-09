ee50818daa3d84f2a9efa9f594db00d8ae787cf5
Roman Meshceryakov <rom_ik@bk.ru>
2015-02-09 04:18:33 +0400

    Update arraymath.js

--- vkAPPfriendmutual/js/arraymath.js
+++ vkAPPfriendmutual/js/arraymath.js
@@ -15,26 +15,14 @@ function ArrayMath() {
 
 	this.Intersection = function(A,B)
 	{
-	    var M = A.length, N = B.length, C = [],
-	        m = 1, n = 1, k = 0, a = 0, b = 0;
-	
-	    for (var i = 1, t = A[0]; i < M; i++)  // Сдвигаем в начало уникальные элементы
-	     { if (A[ i ] !== t)                   // Первые m-элементов будут уникальными
-	        { A[m++] = A[ i ]; t = A[ i ]; }
-	     }   
-	  
-	    for (var i = 1, t = B[0]; i < N; i++)  // Аналогично предыдущему
-	     { if (B[ i ] !== t)
-	        { B[n++] = B[ i ]; t = B[ i ]; }
-	     }       
-	
-	    while (a < m && b < n)                // Заносим в C только те элементы A и B,
-	     { if (A[ a ] < B[ b ]) ++a;          //  которые есть в них обоих
-	       else if (A[ a ] > B[ b ]) ++b;
-	       else С[k++] = A[a++];
+	    var m = A.length, n = B.length, c = 0, C = [];
+	    for (var i = 0; i < m; i++)
+	     { var j = 0, k = 0;
+	       while (B[j] !== A[ i ] && j < n) j++;
+	       while (C[k] !== A[ i ] && k < c) k++;
+	       if (j != n && k == c) C[c++] = A[ i ];
 	     }
-	
-	    return C;
+	   return C;
 	}
 
 	this.Sum = function(A,B)
