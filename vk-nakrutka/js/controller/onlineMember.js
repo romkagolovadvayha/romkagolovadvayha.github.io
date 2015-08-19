ee70e581c1e039caf21290ba129c5f28a7bc937f
romkagolovadvayha <rom_ik@bk.ru>
2015-08-19 16:01:24 +0400

    online vk

---
+++ vk-nakrutka/js/controller/onlineMember.js
@@ -0,0 +1,97 @@
+/**
+ * OnlineMemberCtrl
+ */
+'use strict';
+app.controller('OnlineMemberCtrl', function ($scope, ngToast, $timeout, cfpLoadingBar) {
+
+    $scope.form_post = false;
+    $scope.error_post = false;
+    $scope.count_members = 1000;
+
+    $scope.price = {
+        member: 3 / 100,
+        sex: 2 / 100,
+        country: 1 / 100
+    };
+
+    $scope.group = {
+        name: null,
+        members_count: null,
+        photo_50: null,
+        screen_name: null
+    };
+    $scope.groupFormActive = false;
+
+    $scope._form_post = function () {
+        cfpLoadingBar.start();
+        $scope.error_post = false;
+        $scope.form_post = true;
+        if (!$scope.groupUrl)
+            $scope.error_post = true;
+        if (($scope.count_members == 0) || !$scope.count_members)
+            $scope.error_post = true;
+
+        cfpLoadingBar.complete();
+            if ($scope.error_post)
+                $scope.writeError('Неверно заполнены поля!');
+
+    };
+
+    $scope.reset = function () {
+        $scope.groupFormActive = false;
+    };
+
+    $scope.start = function (response) {
+        cfpLoadingBar.start();
+        var url = $scope.groupUrl;
+        if (url) {
+            if (response && response.session) {
+                if (url.indexOf("com/") >= 0)
+                    url = url.split('com/')[1];
+                VK.Api.call('utils.resolveScreenName', {screen_name: url, v: '5.27'}, function (data) {
+                    if (data.response) {
+                        if (data.response.type == 'group') {
+                            $scope.getGroupsInfo(data.response.object_id);
+                            //$scope.startStats(data.response.object_id);
+                        } else {
+                            cfpLoadingBar.complete();
+                            $scope.writeError('Неверно указана ссылка');
+                        }
+                    }
+                });
+            } else {
+                cfpLoadingBar.complete();
+                VK.Auth.login($scope.start);
+            }
+        } else {
+            cfpLoadingBar.complete();
+            $scope.writeError('Введите ссылку');
+        }
+    };
+
+    $scope.startVK = function () {
+        VK.Auth.getLoginStatus($scope.start);
+    };
+
+
+    $scope.writeError = function (error) {
+        ngToast.success(error);
+        $scope.$digest();
+    };
+
+
+    $scope.groupID = 0;
+    $scope.getGroupsInfo = function (groupID) {
+        VK.Api.call('groups.getById', {group_id: groupID, fields: 'members_count', v: '5.34'}, function (data) {
+            if (data.response) {
+                $scope.group = data.response[0];
+                $scope.groupID = groupID;
+                cfpLoadingBar.complete();
+                $scope.groupFormActive = true;
+                $scope.$digest();
+            }
+        });
+    };
+
+
+});
---
+++ vk-nakrutka/js/scripts.js
@@ -0,0 +1,5 @@
+var app = angular.module('application', ['ngToast', 'chart.js', 'cfp.loadingBar']);
+
+VK.init({
+    apiId: 4965958
+});
