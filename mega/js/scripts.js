c89ed714eb651fd51ee6165e24bdc47735e0ddd9
romkagolovadvayha <rom_ik@bk.ru>
2015-09-11 05:38:25 +0400

    online vk

--- mega/js/scripts.js
+++ mega/js/scripts.js
@@ -5,7 +5,7 @@ $('.photo').click(function () {
     modal_status = !modal_status;
     var element = $(this);
     photo_entity.css('backgroundImage', "url(" + element.attr('data-href') + ")");
-
+    document.location.href='#photo_entity';
     photo_entity.show('hide');
     photo_bg.show();
     //element.attr('data-href');
