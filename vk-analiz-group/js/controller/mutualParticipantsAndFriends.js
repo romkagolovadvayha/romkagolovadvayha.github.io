0322b51b9b2d32e04fe63b51a2d510fc9b0ed05a
romkagolovadvayha <rom_ik@bk.ru>
2015-10-02 17:35:27 +0400

    online vk

---
+++ vk-analiz-group/js/controller/mutualParticipantsAndFriends.js
@@ -0,0 +1,14 @@
+/**
+ * MutualParticipantsAndFriendsCtrl
+ */
+'use strict';
+app.controller('MutualParticipantsAndFriendsCtrl', function ($scope, ngToast, $timeout, cfpLoadingBar) {
+
+    //$('#show-mutual-participants-and-friends').on('shown.bs.tab', function (e) {
+    //    alert();
+    //  //  $scope.$digest();
+    //})
+
+
+
+});
