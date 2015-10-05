2368f80e0761e484d8c074fa380fb27c4045f069
romkagolovadvayha <rom_ik@bk.ru>
2015-10-05 05:41:39 +0400

    frewdfewf

--- driveschool/js/scripts.js
+++ driveschool/js/scripts.js
@@ -92,7 +92,7 @@ $(document).ready(function() {
 				if (target.length) {
 					$('html,body').animate({
 						scrollTop: target.offset().top
-					}, 2000);
+					}, 500);
 					return false;
 				}
 			}
