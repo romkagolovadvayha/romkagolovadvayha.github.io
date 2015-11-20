665b750210fbee1c53e26700a611cf4eea8d09d6
romkagolovadvayha <rom_ik@bk.ru>
2015-11-20 16:11:53 +0400

    frewdfewf

--- vk-analiz-group-2.1/js/scripts.js
+++ vk-analiz-group-2.1/js/scripts.js
@@ -1,12 +1,5 @@
 var app = angular.module('application', ['ngToast', 'chart.js', 'cfp.loadingBar']);
 
-var myGroups = [];
-VK.api('groups.get', {extended: 1, v: '5.40', https: 1, count: 300}, function (data) {
-    if (data.response) {
-        myGroups = data.response.items;
-    }
-});
-
 //VK.init({
 //    apiId: 4965958
 //});
