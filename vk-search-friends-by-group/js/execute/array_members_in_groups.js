597adbf53dd52ff62d7e079f117c6060adc9ffed
romkagolovadvayha <rom_ik@bk.ru>
2015-12-06 22:32:30 +0400

    frewdfewf

--- vk-search-friends-by-group/js/execute/array_members_in_groups.js
+++ vk-search-friends-by-group/js/execute/array_members_in_groups.js
@@ -1,16 +1,30 @@
 var groups_ids = $data$;
-var arr_groups = [];
+var groups_ids = [10827415, 45653506];
+var arr_groups = "";
 
 var i = 0;
-while (i < groups_ids.length)
-{
-    arr_groups[groups_ids[i]] = API.groups.getMembers({
+while (i < groups_ids.length) {
+    var items = API.groups.getMembers({
         "group_id": groups_ids[i],
         "v": "5.27",
         "sort": "id_asc",
         "count": "1000",
-        filter: 'friends'
+        filter: "friends"
     }).items;
+    var j = 0;
+    var items_result = "";
+    while (j < items.length) {
+        if (j == 0)
+            items_result = items[i];
+        else
+            items_result = items_result + ", " + items[i];
+        j = j + 1;
+    }
+    if (i == 0)
+        arr_groups = "{id:" + groups_ids[i] + ", items:" + items + "}";
+    else
+        arr_groups = arr_groups + ", {id:" + groups_ids[i] + ", items:" + items_result + "}";
+    i = i + 1;
 }
 
 return arr_groups;
