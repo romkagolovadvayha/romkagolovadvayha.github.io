5329926440af696a0cc839368d063e7e66ee8575
romkagolovadvayha <rom_ik@bk.ru>
2015-12-07 01:00:04 +0400

    frewdfewf

--- vk-search-friends-by-group/js/controller/searchFriendsByWordFromGroups.js
+++ vk-search-friends-by-group/js/controller/searchFriendsByWordFromGroups.js
@@ -28,19 +28,6 @@ app.controller('SearchFriendsByWordFromGroupsCtrl', function ($scope, ngToast, $
         console.log(code); //
     };
 
-    $scope.invite = function () {
-        VK.callMethod('showInviteBox');
-    };
-    $scope.wall = function () {
-        VK.api('wall.post', {
-            message: 'Всевидящее око! Узнай по слову (Например: "Порно") все группы в которых состоят ваши друзья. https://vk.com/app3278556',
-            attachments: 'photo33610634_350256389, https://vk.com/app3278556',
-            https: 1
-        }, function (data) {
-
-        });
-    };
-
     $(document).bind('keydown', function () {
         if (event.keyCode == 13) {
             $scope.search();
