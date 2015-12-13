a6fe2542beb5110fa5e82a207abc3f9f731709c8
romkagolovadvayha <rom_ik@bk.ru>
2015-12-13 21:59:23 +0400

    frewdfewf

---
+++ vk-analiz-group-2.2/js/controller/profileApp.js
@@ -0,0 +1,26 @@
+/**
+ * ProfileAppCtrl
+ */
+'use strict';
+app.controller('ProfileAppCtrl', function ($scope, ngToast, $timeout, cfpLoadingBar) {
+
+    var getUrlParameter = function getUrlParameter(sParam) {
+        var sPageURL = decodeURIComponent(window.location.search.substring(1)),
+            sURLVariables = sPageURL.split('&'),
+            sParameterName,
+            i;
+
+        for (i = 0; i < sURLVariables.length; i++) {
+            sParameterName = sURLVariables[i].split('=');
+
+            if (sParameterName[0] === sParam) {
+                return sParameterName[1] === undefined ? true : sParameterName[1];
+            }
+        }
+    };
+
+    $scope.user = JSON.parse(getUrlParameter('api_result'));
+    $scope.balance = 0;
+
+
+});
