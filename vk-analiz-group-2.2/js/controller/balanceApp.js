ab8f693de6c9c3d91f6bb078d0aec6caf96ca54f
romkagolovadvayha <rom_ik@bk.ru>
2015-12-13 23:19:34 +0400

    frewdfewf

--- vk-analiz-group-2.2/js/controller/balanceApp.js
+++ vk-analiz-group-2.2/js/controller/balanceApp.js
@@ -5,15 +5,12 @@
 app.controller('BalanceAppCtrl', function ($scope, ngToast, $timeout, cfpLoadingBar) {
 
     console.clear();
-    console.log('error23');
     $scope.showOrderBox = function (item) {
         var params = {
             type: 'item',
             item: item
         };
-        console.log('error1');
         VK.callMethod('showOrderBox', params);
-        console.log('error2');
     };
 
 });
